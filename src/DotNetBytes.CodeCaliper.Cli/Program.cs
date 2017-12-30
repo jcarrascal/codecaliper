a1ea6036b0b56a845c2e79da44b191020a164b13
Julio César Carrascal Urquijo <jcarrascal@gmail.com>
2017-12-30 05:37:40 -0500

    Initial project structure

---
+++ src/DotNetBytes.CodeCaliper.Cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNetBytes.CodeCaliper.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
